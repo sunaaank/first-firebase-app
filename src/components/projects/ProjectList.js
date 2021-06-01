973beb0566bfccf9a92bf696813e855d7389fc46
sunaaank <suna.namgung@gmail.com>
2021-06-01 20:39:13 +0900

    Add : first commit

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function ProjectList() {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content gery-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by the First User</p>
+          <p className="grey-text">3rd September, 2am</p>
+        </div>
+      </div>
+    </div>
+  );
+}
