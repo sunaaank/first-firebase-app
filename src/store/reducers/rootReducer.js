a99dbd85218de0d28904c56604e30c5190856cd0
sunaaank <suna.namgung@gmail.com>
2021-08-09 15:43:49 +0900

    feat: 덕스 패턴으로 friend, timeline reducer 구현

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,24 @@ import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 import cartReducer from './cartReducer';
 import { combineReducers } from 'redux';
+import timelineReducer, {
+  addTimeline,
+  removeTimeline,
+  editTimeline,
+  increaseNextPage,
+} from '../../components/timeline/state';
+import friendReducer, {
+  addFriend,
+  removeFriend,
+  editFriend,
+} from '../../components/friend/state';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
   cartReducer: cartReducer,
+  timeline: timelineReducer,
+  friend: friendReducer,
 });
 
 export default rootReducer;
